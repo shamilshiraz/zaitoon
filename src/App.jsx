78df57479793cc2cdd072913c5aa572b0361f546
shamilshiraz <shamilshiraz0@gmail.com>
2024-12-05 15:10:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Example from './Components/Mobilehero'
+import Nav from './Components/Nav'
+
+function App() {
+
+  return (
+    <>
+    <Nav/>
+<Example/>
+    </>
+  )
+}
+
+export default App
