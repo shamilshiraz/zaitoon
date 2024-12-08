5df444e381e31c8c8691a27e72f8cec1ee70f882
shamilshiraz <shamilshiraz0@gmail.com>
2024-12-09 01:17:44 +0530

    hero

--- src/Components/ProductItem.jsx
+++ src/Components/ProductItem.jsx
@@ -4,15 +4,16 @@ import { Link } from 'react-router-dom'
 
 
 function ProductItem({id,image,name,price}) {
-    
-  return (
-<Link to={`product/${id}`}>
+    const whatsappURL = `https://wa.me/971543135452?text=${encodeURIComponent(`I am interested in ${name}`)}`; 
+     return (
+<div to={`product/${id}`}>
 <div className="overflow-hidden">
     <img src={image[0]} className='hover:scale transition ease-in-out' alt="" />
 </div>
 <p className='pt-3 pb-1 text-sm'>{name}</p>
 <p className='text-sm font-medium'>{price}AED</p>
-</Link>
+<a     href={whatsappURL}   ><button className='border'>BUY</button></a>
+</div>
   )
 }
 
