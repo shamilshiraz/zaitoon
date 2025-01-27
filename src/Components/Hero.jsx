bbbc40eb1ea591d953589ae155e511e6554b61ce
shamilshiraz <shamilshiraz0@gmail.com>
2025-01-27 18:51:32 +0530

    hosting

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -11,7 +11,7 @@ function Hero() {
         <p className='text-l sm:text-2xl'>ZAITOON MOBILES</p>
         <p className='text-3xl sm:text-6xl'>YOUR GO TO PLACE FOR MOBILE PHONES AND GADGETS</p>
         <p>Trust us for essential tools and robust data security, allowing you to focus on growth with confidence.</p>
-        <Link to={'/products'} className='border w-[150px] p-3 place-content-center grid rounded-3xl '>SHOP NOW</Link>
+        <Link to={'/col'} className='border w-[150px] p-3 place-content-center grid rounded-3xl '>SHOP NOW</Link>
       </div>
       {/* <div className=" place-content-center p-12"><Example/></div> */}
     </div>
